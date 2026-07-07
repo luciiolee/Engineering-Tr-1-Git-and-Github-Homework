2f7509f8238480f338552f9bc2537eb792923a97
Thaomi <lluciiole@gmail.com>
2026-07-06 20:39:54 -0700

    moved scenery to assets and imported it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import sceneryImg from './assets/scenery.jpeg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="container">
     <h1 className="title">Yosemite National Park</h1>
-    <img className="scenery-img-styled" src="./scenery.jpg" alt="A beautiful scenery" />
+    <img className="scenery-img" src={sceneryImg} alt="A beautiful scenery" />
       <p className="description">
         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
       </p>
